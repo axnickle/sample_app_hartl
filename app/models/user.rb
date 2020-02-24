a51c8595dc386baa997df676a74a5fef086cbf85
axnickle <onlychangethatmakescents@gmail.com>
2020-02-24 23:44:20 +0000

    create and autheniticate user; not able to assign a new name and save - need update_attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
     
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format:     { with: VALID_EMAIL_REGEX }, #VALID_EMAIL_REGEX is a constant, indicated in ruby
                                                                 #by a name starting with a capital letter
                     uniqueness: true
     has_secure_password
+    validates :password, length: {minimum: 6 }
 end
 
 #presence: true argument is one-lement options hase; 

9be0d99b5adf5b2afa3ff4598c4f07e7c3a7b9e0
axnickle <onlychangethatmakescents@gmail.com>
2020-03-07 21:24:55 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,22 +12,23 @@ class UsersController < ApplicationController
     def create
       #@user = User.new(params[:user]) #not the final implementation; :user - set of information required to create new user, not always unique
       @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save
-      # flash[:success] = "Welcome to the Sample App!"
-      # redirect_to @user
-    else
-      render 'new'
-   end
-end
+      if @user.save
+        #Handle a successful save
+        flash[:success] = "Welcome to the Sample App!" # flash gives it a KEY, which is [:success]
+        redirect_to @user                             #flash tell rails this data should only persist for 1 request
+      else
+        render 'new'
+      end
+    end
 
-private
 
-  def user_params 
-    params.require(:user).permit(:name, :email, :password,     
-                                  :password_confirmation). #strong
-end
-end
+  private
+
+    def user_params 
+      params.require(:user).permit(:name, :email, :password,     
+                                    :password_confirmation) #strong parameter
+    end
+  end
 # debugger #issues commands to figure out the state of the application
   #good practice to put degger close to the code you think might be causing the trouble
-   #inspecting the state of the systedm using byebug for tracking down appliction errors
+  #inspecting the state of the systedm using byebug for tracking down appliction errors
